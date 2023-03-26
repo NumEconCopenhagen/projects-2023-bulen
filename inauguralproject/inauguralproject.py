5acc6be55a5b212b3ba9ae1e43efd9aa67b7f663
crf764 <crf764@alumni.ku.dk>
2023-03-26 13:52:58 +0200

    Spørgsmål 5 version 1.0

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -22,6 +22,8 @@ class HouseholdSpecializationModelClass:
         par.nu = 0.001
         par.epsilon = 1.0
         par.omega = 0.5 
+        par.epsilonf = 1.0
+        par.epsilonm = 1.0
 
         # c. household production
         par.alpha = 0.5
@@ -73,7 +75,7 @@ class HouseholdSpecializationModelClass:
         Q = C**par.omega*H**(1-par.omega)
         utility = np.fmax(Q,1e-8)**(1-par.rho)/(1-par.rho)
 
-        # d. disutlity of work
+        # d. disutility of work
         epsilon_ = 1+1/par.epsilon
         TM = LM+HM
         TF = LF+HF
@@ -172,10 +174,6 @@ class HouseholdSpecializationModelClass:
         pass
          
 
-    def solve_wF_vec(self,discrete=False):
-        """ solve model for vector of female wages """
-
-        pass
 
     def run_regression(self):
         """ run regression """
@@ -238,3 +236,139 @@ class HouseholdSpecializationModelClass:
         return sol
 
         
+    # Model extension
+
+    def calc_utility_ext(self,LM,HM,LF,HF):
+        """ calculate utility """
+
+        par = self.par
+        sol = self.sol
+
+        # a. consumption of market goods
+        C = par.wM*LM + par.wF*LF
+
+        # b. home production
+        
+        if par.sigma == 1:
+            H = HM**(1-par.alpha)*HF**par.alpha
+        elif par.sigma == 0:
+            H = np.minimum(HM,HF)
+        else: 
+            with np.errstate(all='ignore'):
+                H = ((1-par.alpha)*HM**((par.sigma-1)/par.sigma) + par.alpha*HF**((par.sigma-1)/par.sigma))**(par.sigma/(par.sigma-1))
+            
+
+        
+
+        # c. total consumption utility
+        Q = C**par.omega*H**(1-par.omega)
+        utility = np.fmax(Q,1e-8)**(1-par.rho)/(1-par.rho)
+
+        # d. disutility of work
+        epsilon_f = 1+1/par.epsilonf #divergent epsilon parameters
+        epsilon_m = 1+1/par.epsilonm
+        TM = LM+HM
+        TF = LF+HF
+        disutility = par.nu*(TM**epsilon_m/epsilon_m+TF**epsilon_f/epsilon_f)
+        
+        return utility - disutility
+    
+    def solve_ext(self, do_print=False):
+        """ solve model continuously """
+
+        par = self.par
+        sol = self.sol = SimpleNamespace()
+
+        # a. call solver
+
+        constraint1 = lambda x: 24 - (x[0] + x[1])
+        constraint2 = lambda x: 24 - (x[2] + x[3])
+        constraintz = [{'type':'ineq', 'fun': constraint1},{'type':'ineq','fun': constraint2}]
+
+
+        target = [(12,12,12,12)]
+
+        bounds = ((0,24),(0,24),(0,24),(0,24))
+        
+
+        obj = lambda x: -self.calc_utility_ext(x[0],x[1],x[2],x[3])
+    
+        result = optimize.minimize(obj, target, method='SLSQP', bounds=bounds, constraints = constraintz, tol= 10e-34)
+
+        # c. save results
+        sol.LM = result.x[0]
+        sol.HM = result.x[1]
+        sol.LF = result.x[2]
+        sol.HF = result.x[3]
+
+      
+        g = sol.HF/sol.HM
+        
+        sol.g=g
+
+        h = par.wF/par.wM
+
+        sol.h=h
+        
+        return sol
+        pass
+        
+    def run_regression_ext(self):
+        """ run regression """
+
+        par = self.par
+        sol = self.sol = SimpleNamespace()
+
+        y = []
+        
+
+        for k in par.wF_vec: #Solving log(HM/HF) for different values of wF 
+            self.par.wF=k
+            temp=self.solve_ext()
+            y.append(np.log(temp.HF/temp.HM))
+            
+
+
+        x = np.log(par.wF_vec)
+        
+        A = np.vstack([np.ones(x.size),x]).T # Creates coefficient matrix, a column of five 1's as the constants, and a column size=5 from the wF array as beta_1 coefficients
+        sol.beta0,sol.beta1 = np.linalg.lstsq(A,y,rcond=None)[0] #Regressing by least squares with dependent vector=y and independent vector=A
+        sol.y=y
+        
+        return sol
+        
+    
+
+    def deviation_ext(self, epsilonf, sigma):
+        """calculating the sum of squares"""
+        par=self.par
+        
+        par.epsilonf=epsilonf
+        par.sigma=sigma
+        temp = self.run_regression_ext()
+        errors=((par.beta0_target- temp.beta0)**2 + (par.beta1_target + temp.beta1)**2)
+        return errors
+        
+
+
+       
+    def estimate_ext(self,epsilonf=None,sigma=None):
+        """ estimate alpha and sigma """
+        par=self.par
+        
+        sol = self.sol=SimpleNamespace()
+    
+    
+        bounds = ((2.5,3), (0.6,1.4))  # bounds and target chosen by examining 3D plot of sum of squares
+        target = [(2.8, 1)]
+
+            
+        obj = lambda x: self.deviation_ext(x[0], x[1])
+
+        result = optimize.minimize(obj, target, method='Nelder-Mead', bounds=bounds, tol= 10e-17)
+
+        sol.epsilonf=result.x[0]
+        sol.sigma=result.x[1]
+        
+
+        return sol
