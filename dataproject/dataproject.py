d28852b082c64d3bf0b1f5239b7d7d9b4b74d8c6
crf764 <crf764@alumni.ku.dk>
2023-04-15 15:20:39 +0200

    Ryddet op og løst bug

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,16 +1,3 @@
-def keep_regs(df, regs):
-    """ Example function. Keep only the subset regs of regions in data.
-
-    Args:
-        df (pd.DataFrame): pandas dataframe 
-
-    Returns:
-        df (pd.DataFrame): pandas dataframe
-
-    """ 
-    
-    for r in regs:
-        I = df.reg.str.contains(r)
-        df = df.loc[I == False] # keep everything else
-    
-    return df
+def tblfmt(styler):
+    styler.set_caption('Danish murder convictions by age category 1980-2022')
+    return styler
